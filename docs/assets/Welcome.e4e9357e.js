4a464e33c6bf8e092c1841d6ea49daa5b6e090a5
Proladon <proladon@gmail.com>
2022-01-23 16:29:02 +0800

    build to page test

---
+++ docs/assets/Welcome.e4e9357e.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.c63a6278.js";import{o,q as c}from"./vendor.fcebef1d.js";const r={},t={id:"welcome"};function n(s,_){return o(),c("div",t," welcome ")}var f=e(r,[["render",n]]);export{f as default};
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import WindiCSS from 'vite-plugin-windicss'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sz-single-entrance/',
   plugins: [vue(), WindiCSS()],
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
