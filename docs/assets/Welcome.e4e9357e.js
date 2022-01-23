cb36084f2c6d2a256c9639edee66739cd84796d5
Proladon <proladon@gmail.com>
2022-01-23 16:31:45 +0800

    turn router to hash mode

--- docs/assets/Welcome.e4e9357e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.c63a6278.js";import{o,q as c}from"./vendor.fcebef1d.js";const r={},t={id:"welcome"};function n(s,_){return o(),c("div",t," welcome ")}var f=e(r,[["render",n]]);export{f as default};
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
@@ -30,7 +30,7 @@ const routes = [
 
 const router = createRouter({
   routes,
-  history: createWebHistory(),
+  history: createWebHashHistory(),
 })
 
 export default router
