8295f34dbc3bca45cba5f7ee7aa8677a214c3a3c
Proladon <Proladon@gmail.com>
2022-02-03 17:17:32 +0800

    about content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="home">
     <div class="description common-width">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ab blanditiis omnis, velit nostrum officiis aliquam? Possimus perspiciatis pariatur at quasi corporis quaerat eligendi illum ipsa reprehenderit quas. Adipisci, in!
+      <article>
+        <p class="sub-title">歡迎來到 SHELTER ZONE (SZ) 邊境哨站</p>
+        <div>
+          因發現過往許多人根本不看群規或眼盲，與為了因應不同目的而加入伺服器的人
+          同時為了保持伺服器的品質，所以SZ有自己一套加入伺服器的機制。<br>
+        </div>
+        <div>
+          如欲加入伺服器請根據指示與步驟進行操作
+        </div>
+      </article>
+
+      <article>
+        <p class="sub-title">SZ 是一個怎樣的伺服器？</p>
+        <div>
+          SZ是一個綜合技術交流群<br>
+          主要為程式技術、Discord Bot開發、3D/2D技術、資訊技術交流<br>
+          同時也是<a class="text-[#63e2b7]" href="https://www.youtube.com/c/Proladon" target="_blank" rel="noopener noreferrer">Proladon</a>的個人伺服器<br>
+        </div>
+      </article>
     </div>
 
     <n-button
@@ -18,13 +36,12 @@
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from '@vue/runtime-core'
-import { NButton } from 'naive-ui'
-import { useRouter } from 'vue-router'
+import { computed, ref } from '@vue/runtime-core'
+import { useMessage, NButton } from 'naive-ui'
 import { useStore } from 'vuex'
 
-const router = useRouter()
 const store = useStore()
+const message = useMessage()
 const loggedIn = computed(() => store.state.auth.loggedIn)
 const loading = ref(false)
 
@@ -32,17 +49,21 @@ const login = async () => {
   loading.value = true
   if (loggedIn.value) return
   const [dcAuthLink, getLinkErr] = await store.dispatch('getDCAuthorizeUrl')
-  if (getLinkErr) return console.error(getLinkErr)
+  if (getLinkErr) return message.error(getLinkErr)
   location.href = dcAuthLink
 }
-
-onMounted(async () => {
-  console.log('home')
-})
 </script>
 
 <style lang="postcss" scoped>
 .description {
   @apply my-[30px];
+}
+
+article {
+  @apply mb-[30px];
+}
+
+.sub-title {
+  @apply text-md mb-[5px] font-medium;
 }
 </style>
